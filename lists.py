afcf8f9779d92a553c24d821c3b4cf11175185fe
tomasmontesano1 <timontesano@mail.austral.edu.ar>
2025-05-15 19:03:57 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,55 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    l1 = list_to_remove_elements
+    l2 = len(l1)
+    if l2 >= 6:
+        del l1[5]
+    l2 = len(l1)
+    if l2 >= 5:
+        del l1[4]
+    l2 = len(l1)
+    if l2 >= 1:
+        del l1[0]
+    return l1
+# Remove this line and implement
 
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_to_add_elements.append('Yellow')
+    list_to_add_elements.insert(0,'Pink')
+
+
+
+    return list_to_add_elements  # Remove this line and implement
 
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
+    a=[]
+    if list_to_check==a:
+        return True
+    else:
+        return False
+
+
+
+
+    # Remove this line and implement
 
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1)>2 and len(list_to_compare2)>2 and list_to_compare1[2]==list_to_compare2[2]:
+        return True
+    else:
+        return False
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    lista = list_of_lists_to_modify
+    l1 = lista[0]
+    l2 = lista[1]
+    l3 = lista[2]
+    l1 = l1[0:2]
+    l2 = l2[1:4]
+    longitud = len(l3)
+    l3 = l3[longitud-2:longitud]
+    lista =[l1,l2,l3]
+    return lista
+
